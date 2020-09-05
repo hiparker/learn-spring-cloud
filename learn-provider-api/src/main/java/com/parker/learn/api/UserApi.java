75461b9ed453b085ce82caddeca4ac2f1a99316a
Parker <meet.parker@foxmail.com>
2020-09-06 00:56:08 +0800

    更换 spring-boot版本

--- learn-provider-api/src/main/java/com/parker/learn/api/UserApi.java
+++ learn-provider-api/src/main/java/com/parker/learn/api/UserApi.java
@@ -1,10 +1,7 @@
 package com.parker.learn.api;
 
 import com.parker.learn.api.vo.User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @BelongsProject: learn-provider-api
@@ -19,7 +16,15 @@ public interface UserApi {
     @GetMapping("/getUser")
     User getUser();
 
+    /**
+     * 三种 传参格式
+     * 1、 默认                @RequestParam
+     * 2、 指定参数名           @RequestParam(name = "xxx")
+     * 3、 传输一个Json格式对象  @RequestBody
+     * @param userName
+     * @return
+     */
     @PostMapping("/saveUser")
-    User saveUser(@RequestParam String userName);
+    User saveUser(@RequestParam(name = "userName") String userName);
 
 }
